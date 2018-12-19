dc6104e515445c49363d450dfb853d024dbccd42
Justin Carter <justin.w.carter@gmail.com>
2018-12-19 12:03:02 +1100

    Update trigger-lucee-dockerfiles-build.sh

--- trigger-lucee-dockerfiles-build.sh
+++ trigger-lucee-dockerfiles-build.sh
@@ -6,10 +6,10 @@ curl -s "https://api.travis-ci.org/v3/job/${TRAVIS_JOB_ID}/log.txt?deansi=true" 
 
 # get lucee version
 LUCEE_VERSION=$(grep -oP "(?<=\[INFO\] Building Lucee Loader Build )(\d+\.\d+\.\d+\.\d+(.*)?)" travis_output.log)
-echo "LUCEE_VERSION = $LUCEE_VERSION"
+echo "LUCEE_VERSION = $LUCEE_VERSION\n"
 
 # build the travis request body
-REQUEST_BODY=$(cat <<'EOF'
+REQUEST_BODY=$(cat <<EOF
   {
     "request": {
       "message": "Testing automated build for version $LUCEE_VERSION",
@@ -26,7 +26,7 @@ REQUEST_BODY=$(cat <<'EOF'
   }
 EOF
 )
-echo "$REQUEST_BODY"
+echo "REQUEST_BODY = $REQUEST_BODY"
 
 # trigger the lucee-dockerfiles travis job for this lucee version
 curl -v -s -X POST \
